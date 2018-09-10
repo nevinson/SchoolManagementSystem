4aaef9c262a980d16625ba24e69d1ab5be1a180f
Nevinson <nevinson23@gmail.com>
2018-09-10 22:59:15 +0200

    Add application database context.

--- Nevinson.School/Data/ApplicationDbContext.cs
+++ Nevinson.School/Data/ApplicationDbContext.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Nevinson.School.Models;
 
 namespace Nevinson.School.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Models.School> Schools { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Student> Students { get; set; }
     }
 }
