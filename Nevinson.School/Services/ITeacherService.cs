9f68ddabea992b23dac1e987f2e8016ab77d605d
Nevinson <nevinson23@gmail.com>
2018-09-14 23:55:00 +0200

    Add teacher service interface and implementation.

---
+++ Nevinson.School/Services/ITeacherService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Nevinson.School.Models;
+using Nevinson.School.ViewModels;
+
+namespace Nevinson.School.Services
+{
+    public interface ITeacherService
+    {
+        List<Teacher> GetAllTeachers();
+        void AddTeacher(Teacher teacher);
+        Teacher GetSingleTeacherById(int id);
+        void UpdateTeacher(Teacher newTeacher);
+        void DeleteTeacher(int id);
+        List<Student> GetStudentsByTeacherId(int teacherId);
+        TeacherViewModel TeacherDeletionConfirmation(int id);
+        TeacherViewModel TeacherDetails(int id);
+        
+    }
+}
