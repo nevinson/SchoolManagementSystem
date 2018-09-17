c9189b3994fc5de6db1ef60c40249011834766ab
Nevinson <nevinson23@gmail.com>
2018-09-17 21:45:18 +0200

    Add student viewmodels.

---
+++ Nevinson.School/ViewModels/StudentViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Nevinson.School.ViewModels
+{
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+        public string StudentFullName { get; set; }
+        public object StudentName { get; internal set; }
+    }
+}
