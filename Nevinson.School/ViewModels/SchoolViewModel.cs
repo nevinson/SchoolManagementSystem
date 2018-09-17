ea801ce695a3c7c6889ce1ffe06ce0dafda1ddec
Nevinson <nevinson23@gmail.com>
2018-09-17 20:48:23 +0200

    Add school viewmodels.

---
+++ Nevinson.School/ViewModels/SchoolViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Nevinson.School.Models;
+
+namespace Nevinson.School.ViewModels
+{
+    public class SchoolViewModel
+    {
+        public int Id { get; set; }
+        public string SchoolName { get; set; }
+        public List<Teacher> Teachers { get; set; }
+    }
+}
