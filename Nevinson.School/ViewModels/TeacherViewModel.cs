7a992f553c732c480be48a07f60a05ff6ea34dc2
Nevinson <nevinson23@gmail.com>
2018-09-17 21:16:41 +0200

    Add teacher viewmodels.

---
+++ Nevinson.School/ViewModels/TeacherViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Nevinson.School.Models;
+
+namespace Nevinson.School.ViewModels
+{
+    public class TeacherViewModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public List<Student> Students { get; set; }
+        public object TeacherName { get; internal set; }
+    }
+}
